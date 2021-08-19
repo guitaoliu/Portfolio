b7b40b53b349bdb38342c1fcc0862ba1c846ae35
Guitao Liu <guitao.liu@Outlook.com>
2021-08-18 21:52:04 -0400

    fix: fix page without valid react component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
-import theme from './theme'
+import theme from '../theme'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
